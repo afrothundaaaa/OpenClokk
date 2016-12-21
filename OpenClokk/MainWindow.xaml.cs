40aa298541609d83318cb4be6ead26ac29cfd1d9
afrothundaaaa <afrothundaaaa@gmail.com>
2016-12-21 00:48:23 -0600

    v0.1.0.0

--- OpenClokk/MainWindow.xaml.cs
+++ OpenClokk/MainWindow.xaml.cs
@@ -1,28 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Threading;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OpenClokk
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            // Load User Settings
+            this.Loaded += MainWindow_Loaded;
+
+            // Start Timer
+            DispatcherTimer clock = new DispatcherTimer();
+
+            clock.Tick += clock_Tick;
+            clock.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            clock.Start();
+
+            // Save User settings
+            this.Closing += MainWindow_Closing;
+        }
+
+        // Tick Event Handler
+        private void clock_Tick(object sender, EventArgs e)
+        {
+            Time_Textblock.Text = DateTime.Now.ToString("hh:mm:ss");
+            AMPM_Textblock.Text = DateTime.Now.ToString("tt");
+            Date_Textblock.Text = DateTime.Now.ToString("dddd dd MMMM yyyy");
+        }
+
+        // Enable Drag on Left Click
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        // Snap to Corners on Left Double Click
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            base.OnMouseDoubleClick(e);
+
+            var workingArea = System.Windows.SystemParameters.WorkArea;
+
+            if (this.Left == 0)
+            {
+                this.Left = workingArea.Right - this.Width;
+                this.Top = workingArea.Bottom - this.Height;
+            }
+            else
+            {
+                this.Left = 0;
+                this.Top = workingArea.Bottom - this.Height;
+            }
+
+        }
+
+        // Retrieve window settings from Window Load Event
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var workingArea = System.Windows.SystemParameters.WorkArea;
+            
+            // Set Window Location
+            if (Properties.Settings.Default.WindowLocation_Top != 0
+                && Properties.Settings.Default.WindowLocation_Left != 0
+                )
+            {
+                this.Left = Properties.Settings.Default.WindowLocation_Left;
+                this.Top = Properties.Settings.Default.WindowLocation_Top;
+            }
+           else if (Properties.Settings.Default.WindowLocation_Top == 0
+                && Properties.Settings.Default.WindowLocation_Left == 0)
+            {
+                this.Left = workingArea.Right - this.Width;
+                this.Top = workingArea.Bottom - this.Height;
+            }
+
+            if (this.Left >= workingArea.Right
+                && this.Top >= workingArea.Bottom)
+            {
+                // Define Working Area
+                this.Left = workingArea.Right - this.Width;
+                this.Top = workingArea.Bottom - this.Height;
+            }
+        }
+
+        // Window Closing Event
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //Copy Window location to app settings
+            Properties.Settings.Default.WindowLocation_Left = this.Left;
+            Properties.Settings.Default.WindowLocation_Top = this.Top;
+
+            // Save Settings
+            Properties.Settings.Default.Save();
         }
     }
 }
